c4e0892ec6379ac1aea51238593292e1d54842a6
sante-afk <sshyta@mail.ru>
2026-03-31 13:12:00 +0300

    Add localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import InputTask from "./components/InputTask";
 import ButtonAddTask from "./components/ButtonAddTask";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { TaskType } from "./types/TaskType";
 
 function App() {
   const [taskText, setTaskText] = useState("");
-  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [tasks, setTasks] = useState<TaskType[]>(() => {
+    const saveLocalTasks = localStorage.getItem("tasks");
+    if (saveLocalTasks) {
+      return JSON.parse(saveLocalTasks);
+    }
+    return [];
+  });
 
   const handleAddTask = () => {
     const newTask: TaskType = {
@@ -30,6 +36,10 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== taskId));
   }
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+
   return (
     <>
       <div className="taskContainter">
@@ -41,7 +51,7 @@ function App() {
           {tasks.map((task) => {
             return (
               <li key={task.id} className="task">{task.text}
-                <button className="deleteTask" onClick={() => {handleDelTask(task.id)}}>X</button>
+                <button className="deleteTask" onClick={() => { handleDelTask(task.id) }}>X</button>
               </li>
             );
           })}
